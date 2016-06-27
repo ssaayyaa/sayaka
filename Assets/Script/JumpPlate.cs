a245bec9a797162dbadea4f5bb0cdc2a79cc0d69
ssaayyaa <1416069@ed.tus.ac.jp>
2016-06-27 17:19:16 +0900

    no message

---
+++ Assets/Script/JumpPlate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpPlate : MonoBehaviour {
+    public float power = 10;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody rigid = collision.gameObject.GetComponent<Rigidbody>();
+            rigid.AddForce(0, this.power, 0);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
